3a59e79a7653d7eb6b5aa1087278c0959b434b0e
Birdem58 <huzeyfecelep@hotmail.com>
2025-02-11 16:58:25 +0300

    Day0 updates

---
+++ Assets/Scripts/Day_0_Scripts/HideJournalItem.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using prison161.EventBus;
+using PuppetOfShadows.EventBinding;
+using UnityEngine;
+
+public class HideJournalItem : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        EventBus<GetJournal>.Register(new EventBinding<GetJournal>(OnDestroyJournalItem));
+    }
+
+    private void OnDisable()
+    {
+        EventBus<GetJournal>.Deregister(new EventBinding<GetJournal>(OnDestroyJournalItem));
+    }
+    private void OnDestroyJournalItem(GetJournal getJournal)
+    {
+        if(getJournal.journalEnable)
+        {
+            
+            Destroy(gameObject);
+        }
+    }
+}
