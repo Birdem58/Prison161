8ad1e375ea888831d5c03eaaecd47fd3670c3aa7
veiinstone <gulsunnaz@gmail.com>
2025-03-24 14:08:12 +0300

    Revert "SKYBOX GERİ GELDİ YEYYY"
    This reverts commit 4b5343b535ef5cc9e53a64e8efea807030ee1ef3.

--- Assets/Scripts/NPC_Interaction/InteractionHandler.cs
+++ Assets/Scripts/NPC_Interaction/InteractionHandler.cs
@@ -8,21 +8,20 @@ using prison161.EventBus;
 
 public class InteractionHandler : MonoBehaviour
 {
-    // Reference to the interactable component found via raycast.
+    
     IInteraction interaction;
-    // Optional component that can provide an alternate object message.
+   
     InteractMe interactMe;
 
     [SerializeField] private GameObject PlayerInteractionCanvas;
     [SerializeField] private Image crossHair;
-    [SerializeField] private TextMeshProUGUI interactionText;
+    [SerializeField] private TextMeshProUGUI interactTxt;
     [SerializeField] private TextMeshProUGUI reactionMessageText;
 
     private bool CanInteract = false;
     public float rayLength = 3f;
     private RaycastHit hit;
 
-    // This field stores the dialogue interaction once initiated.
     private IInteraction currentDialogueInteraction = null;
 
     void Start()
@@ -36,9 +35,9 @@ public class InteractionHandler : MonoBehaviour
             crossHair.color = Color.white;
         }
 
-        if (interactionText != null)
+        if (interactTxt != null)
         {
-            interactionText.gameObject.SetActive(false);
+            interactTxt.gameObject.SetActive(false);
         }
 
         if (reactionMessageText != null)
@@ -49,14 +48,14 @@ public class InteractionHandler : MonoBehaviour
 
     void FixedUpdate()
     {
-        // When in dialogue mode, we retain the dialogue interaction without updating the raycast.
+        
         if (PlayerState.Instance.GetState() == PlayerState.State.DIALOGUE)
             return;
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hit, rayLength))
         {
-            // Retrieve the IInteraction and optional InteractMe components.
+            
             interaction = hit.collider.GetComponent<IInteraction>();
             interactMe = hit.collider.GetComponent<InteractMe>();
             CanInteract = (interaction != null);
@@ -71,11 +70,11 @@ public class InteractionHandler : MonoBehaviour
 
     void Update()
     {
-        // Clear any stored dialogue interaction if not in dialogue mode.
+       
         if (PlayerState.Instance.GetState() != PlayerState.State.DIALOGUE)
             currentDialogueInteraction = null;
 
-        // In dialogue mode, process dialogue input and hide the interaction UI.
+        
         if (PlayerState.Instance.GetState() == PlayerState.State.DIALOGUE)
         {
             HideInteractionUI();
@@ -86,21 +85,21 @@ public class InteractionHandler : MonoBehaviour
             return;
         }
 
-        // Normal mode: display the appropriate prompt.
+      
         if (CanInteract)
         {
-            // If an InteractMe component is present, display its object message.
+            
             if (interactMe != null)
             {
                 reactionMessageText.text = interactMe.objectMessage;
                 reactionMessageText.gameObject.SetActive(true);
-                interactionText.gameObject.SetActive(false);
+                interactTxt.gameObject.SetActive(false);
             }
-            // Otherwise, display the prompt provided by the interactable object.
+           
             else if (interaction != null)
             {
-                interactionText.text = interaction.InteractionPrompt;
-                interactionText.gameObject.SetActive(true);
+                interactTxt.text = interaction.InteractionPrompt;
+                interactTxt.gameObject.SetActive(true);
                 reactionMessageText.gameObject.SetActive(false);
             }
         }
@@ -109,7 +108,7 @@ public class InteractionHandler : MonoBehaviour
             HideInteractionUI();
         }
 
-        // When F is pressed, trigger the interaction.
+       
         if (CanInteract && Input.GetKeyDown(KeyCode.F))
         {
             interaction.Interact();
@@ -117,19 +116,17 @@ public class InteractionHandler : MonoBehaviour
             
         }
 
-        // Allow the player to hide the UI with Escape.
+       
         if (!CanInteract && Input.GetKeyDown(KeyCode.Escape))
         {
             HideInteractionUI();
         }
     }
 
-    /// <summary>
-    /// Hides both interaction UI elements.
-    /// </summary>
+   
     public void HideInteractionUI()
     {
-        interactionText.gameObject.SetActive(false);
+        interactTxt.gameObject.SetActive(false);
         reactionMessageText.gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/SelectionCharactersScript/SelectionMechanizm.cs
+++ Assets/Scripts/SelectionCharactersScript/SelectionMechanizm.cs
@@ -7,7 +7,6 @@ using UnityEngine.UI;
 
 public class SelectionMechanizm : MonoBehaviour
 {
-    
     public static SelectionMechanizm Instance { get; private set; }
 
     [Header("UI Elemanlarý")]
@@ -15,7 +14,6 @@ public class SelectionMechanizm : MonoBehaviour
     [SerializeField] private TMP_Text _counterText;
     [SerializeField] private Transform _listContent;
     [SerializeField] private GameObject _listItemPrefab;
-    [SerializeField] private Button _confirmButton;
 
     [Header("Seçim Ayarlarý")]
     [SerializeField] private int _maxSelections = 5;
@@ -30,17 +28,16 @@ public class SelectionMechanizm : MonoBehaviour
 
     private void Awake()
     {
+        DOTween.Init();
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject); 
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
             Destroy(gameObject);
         }
-
-        _confirmButton.interactable = false;
         ToggleUI(false);
     }
 
@@ -57,7 +54,7 @@ public class SelectionMechanizm : MonoBehaviour
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
             SpriteRenderer spriteRenderer = hit.collider.GetComponent<SpriteRenderer>();
-        
+
             if (spriteRenderer != null && !_selectedCharacterNames.Contains(hit.collider.name))
             {
                 OnHoverStart(spriteRenderer);
@@ -79,7 +76,6 @@ public class SelectionMechanizm : MonoBehaviour
         _currentHoveredSprite = spriteRenderer;
         _originalSprite = spriteRenderer.sprite;
 
-       
         CharacterSprites charSprites = spriteRenderer.GetComponent<CharacterSprites>();
         if (charSprites != null)
         {
@@ -87,30 +83,35 @@ public class SelectionMechanizm : MonoBehaviour
         }
         else
         {
-          
-            spriteRenderer.sprite = Resources.Load<Sprite>("HoverSprite");
+            Debug.Log("karakterin hover sprite'ý yok");
         }
 
-        transform.DOShakePosition(_shakeDuration, _shakeStrength);
+        
+        spriteRenderer.transform.DOShakePosition(_shakeDuration, _shakeStrength);
     }
 
     private void OnHoverEnd()
     {
         if (_currentHoveredSprite == null) return;
-       
+
         CharacterSprites charSprites = _currentHoveredSprite.GetComponent<CharacterSprites>();
         if (charSprites != null)
         {
-            _currentHoveredSprite.sprite = charSprites.defaultSprite;
+            // Eðer karakter seçili deðilse, normal sprite'a döner
+            if (!_selectedCharacterNames.Contains(_currentHoveredSprite.gameObject.name))
+            {
+                _currentHoveredSprite.sprite = charSprites.defaultSprite;
+                _currentHoveredSprite.color = Color.white;
+            }
         }
         else
         {
             _currentHoveredSprite.sprite = _originalSprite;
         }
+
         _currentHoveredSprite = null;
     }
 
-   
     private void HandleSelectionInput()
     {
         if (Input.GetMouseButtonDown(0))
@@ -118,21 +119,25 @@ public class SelectionMechanizm : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
-                string characterName = hit.collider.name;
-                if (_selectedCharacterNames.Contains(characterName))
+                CharacterSprites selectable = hit.collider.GetComponent<CharacterSprites>();
+
+                if (selectable != null)
                 {
-                    RemoveSelection(characterName);
+                    string characterName = hit.collider.name;
+                    if (_selectedCharacterNames.Contains(characterName))
+                    {
+                        RemoveSelection(characterName);
+                    }
+                    else if (_selectedCharacterNames.Count < _maxSelections)
+                    {
+                        AddSelection(characterName, hit.collider.gameObject);
+                    }
+                    UpdateUI();
                 }
-                else if (_selectedCharacterNames.Count < _maxSelections)
-                {
-                    AddSelection(characterName, hit.collider.gameObject);
-                }
-                UpdateUI();
             }
         }
     }
 
-   
     private void AddSelection(string characterName, GameObject characterObj)
     {
         _selectedCharacterNames.Add(characterName);
@@ -143,17 +148,16 @@ public class SelectionMechanizm : MonoBehaviour
             SpriteRenderer sr = characterObj.GetComponent<SpriteRenderer>();
             if (sr != null)
             {
-                sr.sprite = charSprites.selectedSprite;
+                sr.sprite = charSprites.defaultSprite;
+                sr.DOColor(new Color(132 / 255f, 132 / 255f, 132 / 255f, 1f), 0.3f);
             }
         }
     }
 
-    
     private void RemoveSelection(string characterName)
     {
         _selectedCharacterNames.Remove(characterName);
 
-      
         GameObject characterObj = GameObject.Find(characterName);
         if (characterObj != null)
         {
@@ -164,43 +168,38 @@ public class SelectionMechanizm : MonoBehaviour
                 if (sr != null)
                 {
                     sr.sprite = charSprites.defaultSprite;
+                    sr.DOColor(Color.white, 0.3f);
                 }
             }
         }
     }
 
-   
     private void UpdateUI()
     {
+        // Eski list item'larý temizle
         foreach (var item in _instantiatedItems)
         {
             Destroy(item);
         }
         _instantiatedItems.Clear();
 
+        // Sadece karakter isimlerini içeren list item'larý oluþtur
         foreach (var name in _selectedCharacterNames)
         {
             GameObject listItem = Instantiate(_listItemPrefab, _listContent);
-            listItem.GetComponentInChildren<TMP_Text>().text = name;
-
-           
-            Button removeBtn = listItem.GetComponentInChildren<Button>();
-            removeBtn.onClick.AddListener(() => RemoveSelection(name));
-
+            listItem.GetComponentInChildren<TMP_Text>().text = $"- {name}";
             _instantiatedItems.Add(listItem);
         }
 
         _counterText.text = $"{_selectedCharacterNames.Count}/{_maxSelections}";
-        _confirmButton.interactable = (_selectedCharacterNames.Count == _maxSelections);
     }
 
-   
     public void ToggleUI(bool state)
     {
         _selectionUI.SetActive(state);
+        
     }
 
-  
     public void ConfirmSelection()
     {
         if (_selectedCharacterNames.Count == _maxSelections)
