888a58fcd6b7d73824c0229ce24909c3c5916f7a
veiinstone <118077585+veiinstone@users.noreply.github.com>
2025-06-03 17:21:12 +0300

    Post proccesing effect olayyy

--- Assets/URP-PSX-master/URP-PSX/Assets/Scripts/Dithering/DitheringRenderFeature.cs
+++ Assets/URP-PSX-master/URP-PSX/Assets/Scripts/Dithering/DitheringRenderFeature.cs
@@ -13,32 +13,24 @@ namespace PSX
             ditheringPass = new DitheringPass(RenderPassEvent.BeforeRenderingPostProcessing);
         }
 
-        //ScripstableRendererFeature is an abstract class, you need this method
         public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
         {
-            ditheringPass.Setup(renderer.cameraColorTarget);
             renderer.EnqueuePass(ditheringPass);
         }
     }
-    
-    
+
     public class DitheringPass : ScriptableRenderPass
     {
         private static readonly string shaderPath = "PostEffect/Dithering";
         static readonly string k_RenderTag = "Render Dithering Effects";
         static readonly int MainTexId = Shader.PropertyToID("_MainTex");
         static readonly int TempTargetId = Shader.PropertyToID("_TempTargetDithering");
-        
-        //PROPERTIES
-        static readonly int PatternIndex = Shader.PropertyToID("_PatternIndex");
-        static readonly int DitherThreshold = Shader.PropertyToID("_DitherThreshold");
-        static readonly int DitherStrength = Shader.PropertyToID("_DitherStrength");
-        static readonly int DitherScale = Shader.PropertyToID("_DitherScale");
-        
+
+        static readonly int DitherAmount = Shader.PropertyToID("_DitherStrength"); // shader'daki parametre ismiyle eşleşmeli
+
         Dithering dithering;
         Material ditheringMaterial;
-        RenderTargetIdentifier currentTarget;
-    
+
         public DitheringPass(RenderPassEvent evt)
         {
             renderPassEvent = evt;
@@ -50,7 +42,7 @@ namespace PSX
             }
             this.ditheringMaterial = CoreUtils.CreateEngineMaterial(shader);
         }
-    
+
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
             if (this.ditheringMaterial == null)
@@ -58,42 +50,33 @@ namespace PSX
                 Debug.LogError("Material not created.");
                 return;
             }
-    
+
             if (!renderingData.cameraData.postProcessEnabled) return;
-    
+
             var stack = VolumeManager.instance.stack;
-            
             this.dithering = stack.GetComponent<Dithering>();
             if (this.dithering == null) { return; }
             if (!this.dithering.IsActive()) { return; }
-    
+
             var cmd = CommandBufferPool.Get(k_RenderTag);
             Render(cmd, ref renderingData);
             context.ExecuteCommandBuffer(cmd);
             CommandBufferPool.Release(cmd);
         }
-    
-        public void Setup(in RenderTargetIdentifier currentTarget)
-        {
-            this.currentTarget = currentTarget;
-        }
-    
+
         void Render(CommandBuffer cmd, ref RenderingData renderingData)
         {
             ref var cameraData = ref renderingData.cameraData;
-            var source = currentTarget;
+            var source = cameraData.renderer.cameraColorTarget;
             int destination = TempTargetId;
-    
-            //getting camera width and height 
+
             var w = cameraData.camera.scaledPixelWidth;
             var h = cameraData.camera.scaledPixelHeight;
-            
-            //setting parameters here 
-            cameraData.camera.depthTextureMode = cameraData.camera.depthTextureMode | DepthTextureMode.Depth;
-            this.ditheringMaterial.SetInt(PatternIndex, this.dithering.patternIndex.value);
-            this.ditheringMaterial.SetFloat(DitherThreshold, this.dithering.ditherThreshold.value);
-            this.ditheringMaterial.SetFloat(DitherStrength, this.dithering.ditherStrength.value);
-            this.ditheringMaterial.SetFloat(DitherScale, this.dithering.ditherScale.value);
+
+            cameraData.camera.depthTextureMode |= DepthTextureMode.Depth;
+
+            // Burada ditherStrength kullanılıyor
+            this.ditheringMaterial.SetFloat(DitherAmount, this.dithering.ditherStrength.value);
 
             int shaderPass = 0;
             cmd.SetGlobalTexture(MainTexId, source);
--- Assets/URP-PSX-master/URP-PSX/Assets/Scripts/Pixelation/PixelationRenderFeature.cs
+++ Assets/URP-PSX-master/URP-PSX/Assets/Scripts/Pixelation/PixelationRenderFeature.cs
@@ -13,32 +13,26 @@ namespace PSX
             pixelationPass = new PixelationPass(RenderPassEvent.BeforeRenderingPostProcessing);
         }
 
-        //ScripstableRendererFeature is an abstract class, you need this method
         public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
         {
-            pixelationPass.Setup(renderer.cameraColorTarget);
             renderer.EnqueuePass(pixelationPass);
         }
     }
-    
-    
+
     public class PixelationPass : ScriptableRenderPass
     {
         private static readonly string shaderPath = "PostEffect/Pixelation";
         static readonly string k_RenderTag = "Render Pixelation Effects";
         static readonly int MainTexId = Shader.PropertyToID("_MainTex");
         static readonly int TempTargetId = Shader.PropertyToID("_TempTargetPixelation");
-        
-        //PROPERTIES
+
         static readonly int WidthPixelation = Shader.PropertyToID("_WidthPixelation");
         static readonly int HeightPixelation = Shader.PropertyToID("_HeightPixelation");
         static readonly int ColorPrecison = Shader.PropertyToID("_ColorPrecision");
 
-        
         Pixelation pixelation;
         Material pixelationMaterial;
-        RenderTargetIdentifier currentTarget;
-    
+
         public PixelationPass(RenderPassEvent evt)
         {
             renderPassEvent = evt;
@@ -50,7 +44,7 @@ namespace PSX
             }
             this.pixelationMaterial = CoreUtils.CreateEngineMaterial(shader);
         }
-    
+
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
             if (this.pixelationMaterial == null)
@@ -58,38 +52,30 @@ namespace PSX
                 Debug.LogError("Material not created.");
                 return;
             }
-    
+
             if (!renderingData.cameraData.postProcessEnabled) return;
-    
+
             var stack = VolumeManager.instance.stack;
-            
             this.pixelation = stack.GetComponent<Pixelation>();
             if (this.pixelation == null) { return; }
             if (!this.pixelation.IsActive()) { return; }
-    
+
             var cmd = CommandBufferPool.Get(k_RenderTag);
             Render(cmd, ref renderingData);
             context.ExecuteCommandBuffer(cmd);
             CommandBufferPool.Release(cmd);
         }
-    
-        public void Setup(in RenderTargetIdentifier currentTarget)
-        {
-            this.currentTarget = currentTarget;
-        }
-    
+
         void Render(CommandBuffer cmd, ref RenderingData renderingData)
         {
             ref var cameraData = ref renderingData.cameraData;
-            var source = currentTarget;
+            var source = cameraData.renderer.cameraColorTarget; // Hata düzeltildi
             int destination = TempTargetId;
-    
-            //getting camera width and height 
+
             var w = cameraData.camera.scaledPixelWidth;
             var h = cameraData.camera.scaledPixelHeight;
-            
-            //setting parameters here 
-            cameraData.camera.depthTextureMode = cameraData.camera.depthTextureMode | DepthTextureMode.Depth;
+
+            cameraData.camera.depthTextureMode |= DepthTextureMode.Depth;
             this.pixelationMaterial.SetFloat(WidthPixelation, this.pixelation.widthPixelation.value);
             this.pixelationMaterial.SetFloat(HeightPixelation, this.pixelation.heightPixelation.value);
             this.pixelationMaterial.SetFloat(ColorPrecison, this.pixelation.colorPrecision.value);
